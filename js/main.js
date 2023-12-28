9a7098bd1a47cd9ad1ade40bca215ed5814e15ea
Joya Gamal <105505068+Joya-Gamal@users.noreply.github.com>
2023-12-28 23:58:17 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -57,7 +57,8 @@ function login(){
 
     if(formValidation() ){
         if(checkInput()){
-            window.location.href='./home.html';
+            // window.location.href='./home.html';
+         window.location.pathname ='home.html';
             clearLoginForm()
         }
         else{
